136ecb803d17624b14905541e30714365c9049f5
Mario Irimia <marioirimia@gmail.com>
2023-07-26 14:27:55 +0100

    New project

---
+++ src/com/qa/model/Cat.java
@@ -0,0 +1,6 @@
+package com.qa.model;
+
+public class Cat extends Animal{
+    private String name;
+    private int age;
+}
---
+++ src/com/qa/model/Dog.java
@@ -0,0 +1,6 @@
+package com.qa.model;
+
+public class Dog extends Animal{
+    private String name;
+    private int age;
+}
