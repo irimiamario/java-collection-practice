0965f51ba3e7ed4bf7aeb378cd50aecc4d9ff4ee
Mario Irimia <marioirimia@gmail.com>
2023-07-26 14:42:45 +0100

    error fix

--- src/com/qa/Main.java
+++ src/com/qa/Main.java
@@ -1,10 +1,14 @@
 package com.qa;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Iterator;
 import java.util.TreeMap;
-import java.util.Collections;
+import com.qa.model.Animal;
+import com.qa.model.Cat;
+import com.qa.model.Dog;
+import com.qa.model.Rabbit;
 public class Main {
     public static void main(String[] args) {
         Cat c = new Cat("Whiskers", 10);
